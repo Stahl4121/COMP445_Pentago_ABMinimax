445f0fe0d06bb19406ce637566127b9ee894d798
Stahl4121 <Stahl4121@users.noreply.github.com>
2019-05-02 22:56:55 -0400

    Initial Commit

---
+++ src/Driver_PentagoGame.java
@@ -0,0 +1,13 @@
+
+public class Driver_PentagoGame {
+	public static void main(String[] args) {
+		//PROJECT TODO (Not necessarily in this class):
+		
+		//Create Pentago Game Structure (Board, moves, inputs, check win/lose conditions)
+		//Create UI for game? (Maybe not too much harder than doing a console printed UI)
+		//Enable user to make inputs (get 2-player working)
+		//Write minimax
+		//Write alpha-beta 
+		//Profit
+	}
+}
