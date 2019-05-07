e60463d520d1a8d3e54a319be7309471e77fe21b
Stahl4121 <Stahl4121@users.noreply.github.com>
2019-05-07 15:04:09 -0400

    Minimax fix

--- src/AI.java
+++ src/AI.java
@@ -21,43 +21,45 @@ public class AI extends Player{
 	@Override
 	public Move getMove(Board b) {
 		
-		int moveEncoding = minimax(b, maxDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, color);
+		int moveEncoding = minimax(b, maxDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, color, true);
 		Move m = new Move(moveEncoding, color);
 		
 		return m;
 	}
 	
 
-	public int minimax(Board b, int depth, int alpha, int beta, Status s) {
+	public int minimax(Board b, int depth, int alpha, int beta, Status s, boolean maximize) {
+		ArrayList<Move> possibleMoves;
 		int maxEval;
 		int minEval;
 		int eval;
 		
-		//figure out whether we are the maximizing player
-		boolean maximize = true;
-		if (s != this.color) {
-			maximize = false;
+		
+		if(maximize) {
+			possibleMoves = b.getPossibleMoves(s);
+		}
+		else {
+			//Want to be looking at the other player's moves made
+			Status opposingPlayer = s == Status.BLACK ? Status.WHITE : Status.BLACK;
+			possibleMoves = b.getPossibleMoves(opposingPlayer);
 		}
 		
-		ArrayList<Move> possibleMoves = b.getPossibleMoves(s);
 
 		if (depth == 0 || possibleMoves.isEmpty() || b.winner() != Status.EMPTY) {
 			return b.getBoardFavorability(s);
 		}
 				
 		if (maximize) {
+
 			maxEval = Integer.MIN_VALUE;
 			Move bestMove = null;
-			
-			//Flip the color for the next depth
-			s = s == Status.BLACK ? Status.WHITE : Status.BLACK;
 			
 			for (int i = 0; i < possibleMoves.size(); i++) {
 				
 				Board copy = new Board(b);
 				Move m = possibleMoves.get(i);
 				numMoves++;
-				eval = minimax(copy.movedBoard(m), depth - 1, alpha, beta, s);
+				eval = minimax(copy.movedBoard(m), depth - 1, alpha, beta, s, false);
 				
 				if (eval > maxEval) {
 					maxEval = eval;
@@ -83,9 +85,6 @@ public class AI extends Player{
 		}
 		else {
 			
-			//Flip the color for the next depth
-			s = s == Status.BLACK ? Status.WHITE : Status.BLACK;
-			
 			minEval = Integer.MAX_VALUE;
 			
 			for (int i = 0; i < possibleMoves.size(); i++) {
@@ -93,7 +92,7 @@ public class AI extends Player{
 				Board copy = new Board(b);
 				Move m = possibleMoves.get(i);
 				numMoves++;
-				eval = minimax(copy.movedBoard(m), depth, alpha, beta, s);
+				eval = minimax(copy.movedBoard(m), depth, alpha, beta, s, true);
 				
 				if (eval < minEval) {
 					minEval = eval;
