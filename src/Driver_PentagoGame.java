e09939e9eff10da2dc6b27faf4889574e0d203b1
sarahcalvis <sarahcalvis@gmail.com>
2019-05-07 17:21:17 -0400

    hi

--- src/Driver_PentagoGame.java
+++ src/Driver_PentagoGame.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException; 
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver_PentagoGame {
@@ -29,68 +30,50 @@ public class Driver_PentagoGame {
 		PrintWriter pw = new PrintWriter(f);
 		
 		//figure out what kinda rodeo this is
-		System.out.println("Input 1 for a 2 person game, 2 to play our AI, or 3 to watch our AIs go at it");
+	//	System.out.println("Input 1 for a 2 person game, 2 to play our AI, or 3 to watch our AIs go at it");
 		Scanner s = new Scanner(System.in);
-		int gameType = s.nextInt();
-		
-		//our pentagoers
-		Player miriam;
-		Player sarah;
-		
-		//sarah and miriam fight. both are sentient.
-		if (gameType == 1) {
-			
-			sarah = new Person(Status.WHITE);
-			miriam = new Person(Status.BLACK);
-		}
-		
-		//sarah battles miriam AI
-		if (gameType == 2) {
-			
+//		int gameType = s.nextInt();
+//		
+//		//our pentagoers
+		AI miriam;
+		AI sarah;
+//		
+//		//sarah and miriam fight. both are sentient.
+//		if (gameType == 1) {
+//			
+//			sarah = new Person(Status.WHITE);
+//			miriam = new Person(Status.BLACK);
+//		}
+//		
+//		//sarah battles miriam AI
+//		if (gameType == 2) {
+//			
+//			//get the level we're playing at
+//			System.out.println("Choose your difficulty by inputting a number 1 or greater");
+//			DEPTH = s.nextInt();
+//			pw.println("Depth: " + DEPTH);
+//			
+//			//initialize the players
+//			miriam = new AI(DEPTH, Status.BLACK);
+//			sarah = new Person(Status.WHITE);
+//		} 
+//		
+//		//miriam and sarah, neither sentient, fight
+//		else {
+//			
 			//get the level we're playing at
 			System.out.println("Choose your difficulty by inputting a number 1 or greater");
 			DEPTH = s.nextInt();
 			pw.println("Depth: " + DEPTH);
 			
 			//initialize the players
-			miriam = new AI(DEPTH, Status.BLACK);
-			sarah = new Person(Status.WHITE);
-		} 
-		
-		//miriam and sarah, neither sentient, fight
-		else {
-			
-			//get the level we're playing at
-			System.out.println("Choose your difficulty by inputting a number 1 or greater");
-			DEPTH = s.nextInt();
-			pw.println("Depth: " + DEPTH);
-			
-			//initialize the players
-			miriam = new AI(DEPTH, Status.BLACK);
-			sarah = new AI(DEPTH, Status.WHITE);
-		}
+			miriam = new AI(DEPTH, Status.WHITE);
+			sarah = new AI(DEPTH, Status.BLACK);
+		//}
 				
         while(b.winner()==Status.EMPTY) {  
-        	
-        	//sarah moves
-	        Move r = sarah.getMove(b);
-	        
-	        //print sarah's move
-	        System.out.println(r);
-	        
-	        //make miriam's move
-        	b.makeMove(r);
-        	
-	        // show sarah's move
-	        System.out.println(b.toString());
-	        
-	        //count the AI's moves (specialize for person vs AI)
-	        AI.numMoves = 0;
-	        
-	        //if Sarah won end now
-	        if (b.winner() != Status.EMPTY) { 
-	        	break;
-	        }
+        	 //get start time
+	        double t = System.nanoTime();
 	        
 	        //miriam moves
 	        Move m = miriam.getMove(b);
@@ -105,13 +88,35 @@ public class Driver_PentagoGame {
 	        System.out.println(b.toString());
         	
         	//record moves traversed
-			avgMoves += AI.numMoves;
+			avgMoves += miriam.numMoves;
 			
 			//record another round of the game
 			rounds++;
 			
 			//print to the file
-			pw.println("Move "+rounds+": " + AI.numMoves);
+			System.out.println("Move "+rounds+": " + miriam.numMoves +"\tTime: " + (System.nanoTime() - t)/1000000000);
+			pw.println("Move "+rounds+": " + miriam.numMoves +"\tTime: " + (System.nanoTime() - t)/1000000000);
+			pw.flush();
+      
+	        //count the AI's moves (specialize for person vs AI)
+	        miriam.numMoves = 0;
+	        
+	        //if Sarah won end now
+	        if (b.winner() != Status.EMPTY) { 
+	        	break;
+	        }
+	        
+	      	//sarah moves
+	        Move r = sarah.getMove(b);
+	        
+	        //print sarah's move
+	        System.out.println(r);
+	        
+	        //make miriam's move
+        	b.makeMove(r);
+        	
+	        // show sarah's move
+	        System.out.println(b.toString());
 			
 		}
         
